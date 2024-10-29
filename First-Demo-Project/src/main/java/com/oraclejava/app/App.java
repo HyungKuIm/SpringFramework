2a965b3aa5f4787d547bfd0031f2c50bbd9e74f3
user <user@L340.mshome.net>
2024-10-29 10:09:57 +0900

    spring ioc

--- First-Demo-Project/src/main/java/com/oraclejava/app/App.java
+++ First-Demo-Project/src/main/java/com/oraclejava/app/App.java
@@ -1,10 +1,23 @@
 package com.oraclejava.app;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.oraclejava.model.Message;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+        Message message = context.getBean("message", Message.class);
+        System.out.println("ID:" + message.getMessageId());
+        System.out.println("Message:" + message.getMessage());
+        
+        ((AbstractApplicationContext) context).close();
+        		
     }
 }
